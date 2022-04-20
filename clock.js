cf8ce4eb4bbfdc638a6db7461225625f09f39d5f
mcrooks65 <mfcrooks@gmail.com>
2022-04-19 17:30:36 -0700

    Build basic working clock in clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,28 @@
 console.log(`clock.js is linked into index.html!`);
+
+var showCurrentTime = function()
+{
+    var clock = document.getElementById("clock");
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+    var seconds = currentTime.getSeconds();
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+    var clockTime = hours + ':' + minutes + ':' + seconds;
+    clock.innerText = clockTime;
+}
+
+var start = new Date();
+
+setInterval(function () {
+    showCurrentTime((new Date() - start) % 1000)
+}, 1000);
+
+document.onload = showCurrentTime();
