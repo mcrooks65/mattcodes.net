e44bfbc3a5d5b86e01e14d7891db0b51cb7f08fa
mcrooks65 <mfcrooks@gmail.com>
2022-04-22 14:31:55 -0700

    Add 15 min refresh interval to weather.js

--- weather.js
+++ weather.js
@@ -2,20 +2,23 @@ console.log(`weather.js is linked into index.html!`);
 
 const forecastWeatherApiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=b4da08ea83cd4e3791000620222204&q=93065&days=3&aqi=no&alerts=no' // URL leads to API end-point for Simi Valley weather from weatherapi.com
 
-
 var showWeather = function ()
 {
-    fetch(forecastWeatherApiUrl) // We have a working fetch request!
+    fetch(forecastWeatherApiUrl)
         .then(response => response.json())
         .then(data => {
-        console.log(data)
+        console.log(data) // All weather data is being sent to the Console.
         let currentTempF = data.current.temp_f
         let currentTempC = data.current.temp_c
         let currentLoc = data.location.name
         let currentCondition = data.current.condition.text
-        var currentWeather = document.getElementById("weather");
+        let currentWeather = document.getElementById("weather");
         currentWeather.innerText = `It's currently ${currentTempF}\xB0 Fahrenheit / ${currentTempC}\xB0 Celcius in ${currentLoc}.  Condition is ${currentCondition}.`
     });
 }
+
+setInterval(function () {
+    showWeather();
+}, 900000); // 15 minute refresh for weather info.
 
 document.onload = showWeather();
