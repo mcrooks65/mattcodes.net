82b0577a7277481bb6db36111986626248a503a5
mcrooks65 <mfcrooks@gmail.com>
2022-04-21 17:21:43 -0700

    Add fetch request to weatherapi.com for Simi Valley data

--- weather.js
+++ weather.js
@@ -1,1 +1,7 @@
 console.log(`weather.js is linked into index.html!`);
+
+const url = 'http://api.weatherapi.com/v1/current.json?key=b4da08ea83cd4e3791000620222204&q=93065&aqi=no' // URL leads to API end-point for Simi Valley weather from weatherapi.com
+
+fetch(url) // We have a working fetch request!
+.then(response => response.json())
+.then(data => console.log(data));
