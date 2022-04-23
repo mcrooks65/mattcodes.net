73d36dd5e45ab5673129f47172947a1938bd616c
mcrooks65 <mfcrooks@gmail.com>
2022-04-22 21:07:19 -0700

    Add windStatus to fetch request

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 // Improvements to be made to this script:
 // 1) Add more information for current weather conditions.  Wind would be useful since that seems to be a problem here in Simi Valley.
-// 2) 3-Day Forecast information is available, let's make use of it.
+// 2) 3-Day Forecast information is available, let's make use of it.  
 // 3) Allow user to enter their zipcode to fetch their own local weather.
 
 const forecastWeatherApiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=b4da08ea83cd4e3791000620222204&q=93065&days=3&aqi=no&alerts=no' // URL leads to API end-point for Simi Valley weather from weatherapi.com
@@ -15,8 +15,13 @@ var showWeather = function ()
         let currentTempC = data.current.temp_c
         let currentLoc = data.location.name
         let currentCondition = data.current.condition.text
-        let currentWeather = document.getElementById("weather");
+        let currentWeather = document.getElementById("currentWeather");
         currentWeather.innerText = `It's currently ${currentTempF}\xB0 Fahrenheit / ${currentTempC}\xB0 Celcius in ${currentLoc}.  Condition is ${currentCondition}.`
+        let windMPH = data.current.wind_mph
+        let gustMPH = data.current.gust_mph
+        let windDir = data.current.wind_dir
+        let windStatus = document.getElementById("windStatus")
+        windStatus.innerText = `The wind is heading ${windDir} w/ speed varying from ${windMPH} to ${gustMPH} MPH.`
     });
 }
 
