da6fe771445109178850a21a783bb8aa4712c605
mcrooks65 <mfcrooks@gmail.com>
2022-04-23 17:35:10 -0700

    Add day 2 and 3 forecasts to weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 // Improvements to be made to this script:
-// 1) Add more information for current weather conditions.  Wind would be useful since that seems to be a problem here in Simi Valley.
-// 2) 3-Day Forecast information is available, let's make use of it.  
+// 1) Add more information for current weather conditions.  Wind would be useful since that seems to be a problem here in Simi Valley.  (DONE)
+// 2) 3-Day Forecast information is available, let's make use of it.  (DONE)
 // 3) Allow user to enter their zipcode to fetch their own local weather.
 
 const forecastWeatherApiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=b4da08ea83cd4e3791000620222204&q=93065&days=3&aqi=no&alerts=no' // URL leads to API end-point for Simi Valley weather from weatherapi.com
@@ -37,32 +37,32 @@ var showWeather = function ()
         let forecastWeather1 = document.getElementById("forecastWeather1");
         forecastWeather1.innerText = `Today, ${forecastDay1Name}, the forecast looks ${day1Condition}.  Expect average temperatures to be ${day1AverageTempF}\xB0F / ${day1AverageTempC}\xB0C with a high of ${day1HighTempF}\xB0F / ${day1HighTempC}\xB0C and low of ${day1LowTempF}\xB0F / ${day1LowTempC}\xB0C`
 
-    //     let forecastData2 = data.forecast.forecastday[1]
-    //     let forecastDay2Name = getDayFromDate(forecastData2.date) 
-    //     let forecastDay2 = forecastData2.day
-    //     let day2AverageTempC = forecastDay2.avgtemp_c
-    //     let day2AverageTempF = forecastDay2.avgtemp_f
-    //     let day2Condition = forecastDay2.condition.text
-    //     let day2LowTempF = forecastDay2.mintemp_f
-    //     let day2LowTempC = forecastDay2.mintemp_c
-    //     let day2HighTempF = forecastDay2.maxtemp_f
-    //     let day2HighTempC = forecastDay2.maxtemp_c
-    //     let forecastWeather2 = document.getElementById("forecastWeather2");
-    //     forecastWeather2.innerText = `For ${forecastDay2Name} the forecast looks ${day2Condition}.  Expect average temperatures to be ${day2AverageTempF}\xB0F / ${day2AverageTempC}\xB0C with a high of ${day2HighTempF}\xB0F / ${day2HighTempC}\xB0C and low of ${day2LowTempF}\xB0F / ${day2LowTempC}\xB0C`
+        let forecastData2 = data.forecast.forecastday[1]
+        let forecastDay2Name = getDayFromDate(forecastData2.date) 
+        let forecastDay2 = forecastData2.day
+        let day2AverageTempC = forecastDay2.avgtemp_c
+        let day2AverageTempF = forecastDay2.avgtemp_f
+        let day2Condition = forecastDay2.condition.text
+        let day2LowTempF = forecastDay2.mintemp_f
+        let day2LowTempC = forecastDay2.mintemp_c
+        let day2HighTempF = forecastDay2.maxtemp_f
+        let day2HighTempC = forecastDay2.maxtemp_c
+        let forecastWeather2 = document.getElementById("forecastWeather2");
+        forecastWeather2.innerText = `For ${forecastDay2Name} the forecast looks ${day2Condition}.  Expect average temperatures to be ${day2AverageTempF}\xB0F / ${day2AverageTempC}\xB0C with a high of ${day2HighTempF}\xB0F / ${day2HighTempC}\xB0C and low of ${day2LowTempF}\xB0F / ${day2LowTempC}\xB0C`
 
-    //     let forecastData3 = data.forecast.forecastday[2]
-    //     let forecastDay3Name = getDayFromDate(forecastData3.date) 
-    //     let forecastDay3 = forecastData3.day
-    //     let day3AverageTempC = forecastDay3.avgtemp_c
-    //     let day3AverageTempF = forecastDay3.avgtemp_f
-    //     let day3Condition = forecastDay3.condition.text
-    //     let day3LowTempF = forecastDay3.mintemp_f
-    //     let day3LowTempC = forecastDay3.mintemp_c
-    //     let day3HighTempF = forecastDay3.maxtemp_f
-    //     let day3HighTempC = forecastDay3.maxtemp_c
-    //     let forecastWeather3 = document.getElementById("forecastWeather3");
-    //     forecastWeather3.innerText = `On ${forecastDay3Name} the forecast looks ${day3Condition}.  Expect average temperatures to be ${day3AverageTempF}\xB0F / ${day3AverageTempC}\xB0C with a high of ${day3HighTempF}\xB0F / ${day3HighTempC}\xB0C and low of ${day3LowTempF}\xB0F / ${day3LowTempC}\xB0C`
-    // });
+        let forecastData3 = data.forecast.forecastday[2]
+        let forecastDay3Name = getDayFromDate(forecastData3.date) 
+        let forecastDay3 = forecastData3.day
+        let day3AverageTempC = forecastDay3.avgtemp_c
+        let day3AverageTempF = forecastDay3.avgtemp_f
+        let day3Condition = forecastDay3.condition.text
+        let day3LowTempF = forecastDay3.mintemp_f
+        let day3LowTempC = forecastDay3.mintemp_c
+        let day3HighTempF = forecastDay3.maxtemp_f
+        let day3HighTempC = forecastDay3.maxtemp_c
+        let forecastWeather3 = document.getElementById("forecastWeather3");
+        forecastWeather3.innerText = `On ${forecastDay3Name} the forecast looks ${day3Condition}.  Expect average temperatures to be ${day3AverageTempF}\xB0F / ${day3AverageTempC}\xB0C with a high of ${day3HighTempF}\xB0F / ${day3HighTempC}\xB0C and low of ${day3LowTempF}\xB0F / ${day3LowTempC}\xB0C`
+    });
 }
 
 function getDayFromDate(datestring) {
