fd3e29217125b376bc8bd66eef290221b8a42e3e
mcrooks65 <mfcrooks@gmail.com>
2022-04-27 10:38:53 -0700

    Add event listener for task submit and create loadTasks() in todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,29 @@
-console.log("todo.js is successfully linked to index.html")
+/* Borrowing heavily from https://www.tutorialstonight.com/to-do-list-javascript.php - Has a thorough walk-through on localStorage which I wanted to review. */
+
+// On app load, get all tasks from localStorage
+window.onload = loadTasks;
+
+// On form submit add task
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  addTask();
+});
+
+function loadTasks() {
+  // check if localStorage has any tasks
+  // if not then return
+  if (localStorage.getItem("tasks") == null) return;
+
+  // Get the tasks from localStorage and convert it to an array
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+
+  // Loop through the tasks and add them to the list
+  tasks.forEach(task => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+      <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+      <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    list.insertBefore(li, list.children[0]);
+  });
+}
