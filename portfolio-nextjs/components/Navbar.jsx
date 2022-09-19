7904d9a847eb4ebe4595998397cba3e96f177770
mcrooks65 <mfcrooks@gmail.com>
2022-09-19 00:01:34 -0700

    Setup Navbar with logo

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>NAVBAR TEST</div>
+    <div className='fixed w-full h-20 shadow-xl z[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src="/assets/navLogo.png" alt="/" width='100' height='50'/>
+      </div>
+    </div>
   )
 }
 
