0224c4de0a74b2102193fc637d5aab5971d9326a
mcrooks65 <mfcrooks@gmail.com>
2022-09-19 16:03:30 -0700

    Finish stubbing out small media menu

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineClose, AiOutlineMail} from 'react-icons/ai'
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
@@ -38,14 +40,53 @@ const Navbar = () => {
       </div>
       <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
         <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
-        <div>
-          <div className='flex w-full items-center justify-between'>
-            <Image src="/assets/navLogo.png" alt="/" width='87' height='35'/>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
-              <AiOutlineClose />
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Image src="/assets/navLogo.png" alt="/" width='87' height='35'/>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w [90%] py-4'>Insert Clever Tagline Here</p>
+            </div>
+            <div className='py-4 flex flex-col'>
+              <ul className='uppercase'>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Home</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>About</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Skills</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Projects</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Contact</li>
+                </Link>
+              </ul>
+              <div className='pt-40'>
+                <p className='uppercase tracking-widest text-[#5651e6]'>Let's connect!</p>
+                <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <AiOutlineMail  />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <BsFillPersonLinesFill  />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
