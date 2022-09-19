c9716904772b3349e83044c3c6196bba2b68affd
mcrooks65 <mfcrooks@gmail.com>
2022-09-19 14:57:22 -0700

    Add links to Navbar

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,11 +1,36 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image src="/assets/navLogo.png" alt="/" width='100' height='50'/>
+        <Image 
+          src="/assets/navLogo.png" 
+          alt="/" 
+          width='100' 
+          height='50'
+        />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   )
