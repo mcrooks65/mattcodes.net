f920e1683ab1281ead1bcd4f915751432716c980
mcrooks65 <mfcrooks@gmail.com>
2022-09-19 15:09:52 -0700

    Create seperate nav menu for smaller media devices

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -30,6 +31,21 @@ const Navbar = () => {
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+        <div>
+          <div className='flex w-full items-center justify-between'>
+            <Image src="/assets/navLogo.png" alt="/" width='87' height='35'/>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <AiOutlineClose />
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
