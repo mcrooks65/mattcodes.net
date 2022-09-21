c147a637d352f2447ce083f7bfe05c8c4c7ab4f8
mcrooks65 <mfcrooks@gmail.com>
2022-09-20 23:52:08 -0700

    Set menu button to cursor-pointer to match close button

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
-          <div onClick={handleNav} className='md:hidden'>
+          <div onClick={handleNav} className='md:hidden cursor-pointer'>
             <AiOutlineMenu size={25} />
           </div>
         </div>
