2f42f55977d32ae6565bd2092c15b3adc5054aed
mcrooks65 <mfcrooks@gmail.com>
2022-09-20 17:06:21 -0700

    Create and hookin Main.jsx component

---
+++ portfolio-nextjs/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-width-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>STILL NEED A COOL CATCHPHRASE OR TAGLINE HERE</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
