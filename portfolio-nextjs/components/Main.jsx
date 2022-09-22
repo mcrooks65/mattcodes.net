c0072e0cfa2c3532fadc92ec4b6f881f7c3118de
mcrooks65 <mfcrooks@gmail.com>
2022-09-22 12:05:09 -0700

    Fix apostrophes with html entities

--- portfolio-nextjs/components/Main.jsx
+++ portfolio-nextjs/components/Main.jsx
@@ -9,13 +9,13 @@ const Main = () => {
                     STILL NEED A COOL CATCHPHRASE OR TAGLINE HERE
                 </p>
                 <h1 className='py-4 text-gray-700'> 
-                    Hi, I'm <span className='text-[#5651e5]'>Matt</span>
+                    Hi, I&#39;m <span className='text-[#5651e5]'>Matt</span>
                 </h1>
                 <h1 className='py-4 text-gray-700'> 
                     A Full-Stack Web Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-2-[70%] m-auto'>
-                    I'm a full-stack web developer specializing in building and designing clean user-friendly digital experiences.  Currently I'm focused on building responsive front-end web applications while maintaining my own back-end API's for various projects.
+                    I&#39;m a full-stack web developer specializing in building and designing clean user-friendly digital experiences.  Currently I&#39;m focused on building responsive front-end web applications while maintaining my own back-end API&#39;s for various projects.
                 </p>
             </div>
         </div>
