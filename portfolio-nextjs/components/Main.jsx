f28752e96d6b205c1170741fbceed9fc7c1258cc
mcrooks65 <mfcrooks@gmail.com>
2022-09-22 11:59:44 -0700

    Add content to Main.jsx

--- portfolio-nextjs/components/Main.jsx
+++ portfolio-nextjs/components/Main.jsx
@@ -5,7 +5,18 @@ const Main = () => {
     <div className='w-full h-screen text-center'>
         <div className='max-width-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase text-sm tracking-widest text-gray-600'>STILL NEED A COOL CATCHPHRASE OR TAGLINE HERE</p>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>
+                    STILL NEED A COOL CATCHPHRASE OR TAGLINE HERE
+                </p>
+                <h1 className='py-4 text-gray-700'> 
+                    Hi, I'm <span className='text-[#5651e5]'>Matt</span>
+                </h1>
+                <h1 className='py-4 text-gray-700'> 
+                    A Full-Stack Web Developer
+                </h1>
+                <p className='py-4 text-gray-600 max-2-[70%] m-auto'>
+                    I'm a full-stack web developer specializing in building and designing clean user-friendly digital experiences.  Currently I'm focused on building responsive front-end web applications while maintaining my own back-end API's for various projects.
+                </p>
             </div>
         </div>
     </div>
