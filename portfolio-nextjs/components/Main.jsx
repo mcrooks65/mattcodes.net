5ab696c795c5670b1bef4eafdcce91b7ad286ed5
mcrooks65 <mfcrooks@gmail.com>
2022-09-23 12:43:19 -0700

    Add Shari's edits to Main.jsx

--- portfolio-nextjs/components/Main.jsx
+++ portfolio-nextjs/components/Main.jsx
@@ -18,7 +18,7 @@ const Main = () => {
                     A Full-Stack Web Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-2-[70%] m-auto'>
-                    I&#39;m a full-stack web developer specializing in building and designing clean user-friendly digital experiences.  Currently I&#39;m focused on building responsive front-end web applications while maintaining my own back-end API&#39;s for various projects.
+                    I&#39;m a Full-Stack Web Developer specializing in building and designing clean, user-friendly digital experiences.  Currently, I&#39;m focused on building responsive front-end web applications, while maintaining my own back-end API&#39;s for various projects.
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
