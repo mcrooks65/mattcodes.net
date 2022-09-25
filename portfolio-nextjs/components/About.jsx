599a335ebf6cfd576884ffe8e405860be9689f70
mcrooks65 <mfcrooks@gmail.com>
2022-09-25 16:14:18 -0700

    Copy added to About.jsx

--- portfolio-nextjs/components/About.jsx
+++ portfolio-nextjs/components/About.jsx
@@ -10,7 +10,12 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                 <h2 className='py-4'>Who I Am</h2>
-                <p className='py-2 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='py-2 text-gray-600'>
+                  / / WebDev | GameDev | Project Management
+                </p>
+                <p className='py-2 text-gray-600'>
+                  Though i've recently become a Full-Stack Web Developer, I've always had a fascination with technology.  From a young age my brother and I had a passion for video games, to the point that we would modify the code of open source games and start prototyping our own.  Eventually we took this passion to taught ourselves the neccessary technologies to become indepedent game developers, culminating in the release of our game FreeHolder on Steam.  
+                </p>
                 <p className='py-2 text-gray-600'>
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                 </p>
