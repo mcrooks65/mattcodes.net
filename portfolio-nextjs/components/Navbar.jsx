223cd7dcb1d11d354b1c799393509e31614b4515
mcrooks65 <mfcrooks@gmail.com>
2022-09-27 17:49:25 -0700

    Make navlogo into Link to top of page

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -15,12 +15,15 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image 
-          src="/assets/navLogo.png" 
-          alt="/" 
-          width='100' 
-          height='50'
-        />
+        <Link href='/'>
+          <Image className='cursor-pointer'
+            src="/assets/navLogo.png" 
+            alt="/" 
+            width='100' 
+            height='50'
+          />
+        </Link>
+        
         <div>
           <ul className='hidden md:flex'>
             <Link href='/'>
