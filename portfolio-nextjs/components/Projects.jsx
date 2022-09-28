557efd1933b82c2cacd7f0d44f038b23c6a5fad0
mcrooks65 <mfcrooks@gmail.com>
2022-09-27 17:54:10 -0700

    Create projects.jsx component

---
+++ portfolio-nextjs/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='w-full'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+            <p>Projects</p>
+            <h2>What I've Built</h2>
+
+        </div>
+    </div>
+  )
+}
+
+export default Projects
