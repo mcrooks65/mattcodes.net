71e5f457c8fdd03d8f8684c248a9f6ae96cdbe3e
mcrooks65 <mfcrooks@gmail.com>
2022-09-29 15:08:51 -0700

    Add project image and apply tailwind styling

--- portfolio-nextjs/components/Projects.jsx
+++ portfolio-nextjs/components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Projects = () => {
   return (
@@ -7,7 +9,16 @@ const Projects = () => {
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2 className='py-4'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
-
+                <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+                    <Image className='rounded-xl group-hover:opacity-10' src='/assets/projects/FlagNote-React.jpg'width='800' height='450' alt='/'/>
+                    <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+                        <h3 className='text-2xl text-white tracking-wider text-consectetur'>FlagNote</h3>
+                        <p className='pb-4 pt-2 text-white text-center'>React JS</p>
+                        <Link href='/'>
+                            <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg'>More Info</p>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
