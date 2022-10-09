6841dfe6bc0379d8593053164951717a52bec62d
mcrooks65 <mfcrooks@gmail.com>
2022-10-08 17:50:28 -0700

    Change contrasting color theme to blue/white, w/ green flair

--- portfolio-nextjs/components/About.jsx
+++ portfolio-nextjs/components/About.jsx
@@ -8,19 +8,19 @@ const About = () => {
     <div id='about'className='w-full md:h-screen p-2 flex items-center py-16'>
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
             <div className='col-span-2'>
-                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+                <p className='uppercase text-xl tracking-widest text-green-600'>About</p>
                 <h2 className='py-4'>Who I Am</h2>
-                <p className='py-2 text-gray-600'>
+                <p className='py-2 text-white'>
                   / / WebDev | GameDev | Project Management
                 </p>
-                <p className='py-2 text-gray-600'>
+                <p className='py-2 text-white'>
                   I've always had a fascination with technology.  From a young age, I had a passion for video games. I didn't just enjoy playing the games, but wanted to understand how they worked and how to modify the code to start prototyping my own.
                   My brother had the same interest in video games as I did. We took this fascination and taught ourselves the neccessary technologies to become indepedent game developers, culminating in the release of our game FreeHolder on Steam.  
                 </p>
-                <p className='py-2 text-gray-600'>
+                <p className='py-2 text-white'>
                   Developing and finding a market for FreeHolder has been a big accomplishment, but in a sea of indie game competition we remain small fish, and so I had to find other work to make ends meet and further fund my gamedev ambitions.  I entered the gig economy driving for Lyft and got very comfortable communicating and developing rapport with strangers and occassional regulars. Through this experience, I gained a good understanding of what constitutes excellent customer service.  However, the gig economy wasn't exactly meeting my ambitions.  I wanted to further develop my coding skills  and set out to complete Flatirons School's Software Engineering program.  
                 </p>
-                <p className='py-2 text-gray-600'>
+                <p className='py-2 text-white'>
                   Flatiron's coding bootcamp was a truly immersive experience.  It provided me with the skills and the confidence to build web applications that are clean, efficient, and user-friendly.   This full stack education included programming fundamentals for HTML, CSS, OOP, Ruby, JavaScript, SQL, and ORM. I also learned web frameworks such as Rails and Sinatra and front-end frameworks like React, Redux, and JSON.  Designing and developing my porfolio projects with these technologies has prepped me for a proper engineering role where I can continue to build my skills.  
                 </p>
                 <Link href='/#projects'>
--- portfolio-nextjs/components/Main.jsx
+++ portfolio-nextjs/components/Main.jsx
@@ -8,29 +8,29 @@ const Main = () => {
     <div className='w-full h-screen text-center'>
         <div className='max-width-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase text-sm tracking-widest text-gray-600'>
+                <p className='uppercase text-sm tracking-widest text-white'>
                     STILL NEED A COOL CATCHPHRASE OR TAGLINE HERE
                 </p>
-                <h1 className='py-4 text-gray-700'> 
-                    Hi, I&#39;m <span className='text-[#5651e5]'>Matt</span>
+                <h1 className='py-4 text-white'> 
+                    Hi, I&#39;m <span className='text-green-600'>Matt</span>
                 </h1>
-                <h1 className='py-4 text-gray-700'> 
+                <h1 className='py-4 text-white'> 
                     A Full-Stack Web Developer
                 </h1>
-                <p className='py-4 text-gray-600 max-2-[70%] m-auto'>
+                <p className='py-4 text-white max-2-[70%] m-auto'>
                     I&#39;m a Full-Stack Web Developer specializing in building and designing clean, user-friendly digital experiences.  Currently, I&#39;m focused on building responsive front-end web applications, while maintaining my own back-end API&#39;s for various projects.
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <FaLinkedinIn />
                     </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <FaGithub />
                     </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineMail />
                     </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <BsFillPersonLinesFill />
                     </div>
                 </div>
