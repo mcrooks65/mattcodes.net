ada62ac112d392e7a0d3bbee8757b2ce89ca13a8
mcrooks65 <mfcrooks@gmail.com>
2022-10-12 14:12:01 -0700

    Add in anchor id and header for Contact component

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id='contact' className='w-full lg:h-screen'>
+        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+            <p className='text-xl tracking-widest uppercase text-green-600'>Contact</p>
+            <h2 className='py-4'>Get In Touch</h2>
+        </div>
+    </div>
   )
 }
 
