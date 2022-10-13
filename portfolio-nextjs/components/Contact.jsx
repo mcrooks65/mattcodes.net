7dda80c6a78904f2b4f533b04a018f4238fcb1aa
mcrooks65 <mfcrooks@gmail.com>
2022-10-13 13:10:13 -0700

    Add contact image to Contact.jsx component

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -6,6 +6,15 @@ const Contact = () => {
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
             <p className='text-xl tracking-widest uppercase text-green-600'>Contact</p>
             <h2 className='py-4'>Get In Touch</h2>
+            <div className='grid lg:grid-cols-5 gap-8'>
+                <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+                    <div className='lg:p-4 h-full'>
+                        <div>
+                            <img className='rounded-xl hover:scale-105 ease-in duration-300' src='\assets\contact.jpg' ></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
