f7f1888086906320ddb4f88ca5c9defcd1a17c8a
mcrooks65 <mfcrooks@gmail.com>
2022-10-13 13:50:42 -0700

    Contacts form partial completion

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 const Contact = () => {
   return (
@@ -11,6 +14,51 @@ const Contact = () => {
                     <div className='lg:p-4 h-full'>
                         <div>
                             <img className='rounded-xl hover:scale-105 ease-in duration-300' src='\assets\contact.jpg' ></img>
+                        </div>
+                        <div>
+                            <h2 className='py-2'>Matthew Crooks</h2>
+                            <p>Full-Stack Developer</p>
+                            <p className='py-4'>I am available for freelance or full-time positions.  Contact me and let's talk.</p>
+                        </div>
+                        <div>
+                            <p className='uppercase py-8'>Connect with Me</p>
+                            <div className='flex items-center justify-between py-4'>
+                                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaGithub />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <AiOutlineMail />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-200 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <BsFillPersonLinesFill />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* right */}
+                    <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl py-4'>
+                        <div className='py-4'>
+                            <form>
+                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Name</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Phone Number</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Email</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email'/>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
