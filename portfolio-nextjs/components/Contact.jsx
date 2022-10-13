71810ee2b1d11da131881e41b0144fafb77c03a0
mcrooks65 <mfcrooks@gmail.com>
2022-10-13 14:39:09 -0700

    Fixed left-right form adjustment to larger screen size

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -7,8 +7,12 @@ const Contact = () => {
   return (
     <div id='contact' className='w-full lg:h-screen'>
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
-            <p className='text-xl tracking-widest uppercase text-green-600'>Contact</p>
-            <h2 className='py-4'>Get In Touch</h2>
+            <p className='text-xl tracking-widest uppercase text-green-600'>
+                Contact
+            </p>
+            <h2 className='py-4'>
+                Get In Touch
+            </h2>
             <div className='grid lg:grid-cols-5 gap-8'>
                 {/* Left */}
                 <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
@@ -38,59 +42,58 @@ const Contact = () => {
                                 </div>
                             </div>
                         </div>
-                    {/* Right */}
-                    <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
-                        <div className='py-4'>
-                            <form>
-                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                                <div className='flex flex-col'>
-                                    <label className='uppercase text-sm py-2'>Name</label>
-                                    <input
-                                    className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='text'
-                                    name='name'
-                                    />
-                                </div>
-                                <div className='flex flex-col'>
-                                    <label className='uppercase text-sm py-2'>
-                                    Phone Number
-                                    </label>
-                                    <input
-                                    className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='text'
-                                    name='phone'
-                                    />
-                                </div>
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                <label className='uppercase text-sm py-2'>Email</label>
-                                <input
-                                    className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='email'
-                                    name='email'
-                                />
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                <label className='uppercase text-sm py-2'>Subject</label>
-                                <input
-                                    className='border-2 rounded-lg p-3 flex border-gray-300'
-                                    type='text'
-                                    name='subject'
-                                />
-                                </div>
-                                <div className='flex flex-col py-2'>
-                                <label className='uppercase text-sm py-2'>Message</label>
-                                <textarea
-                                    className='border-2 rounded-lg p-3 border-gray-300'
-                                    rows='10'
-                                    name='message'
-                                ></textarea>
-                                </div>
-                            </form>
-                        </div>
                     </div>
+                </div>
+                {/* Right */}
+                <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+                    <div className='py-4'>
+                        <form>
+                            <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                            <div className='flex flex-col'>
+                                <label className='uppercase text-sm py-2'>Name</label>
+                                <input
+                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                type='text'
+                                name='name'
+                                />
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='uppercase text-sm py-2'>
+                                Phone Number
+                                </label>
+                                <input
+                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                type='text'
+                                name='phone'
+                                />
+                            </div>
+                            </div>
+                            <div className='flex flex-col py-2'>
+                            <label className='uppercase text-sm py-2'>Email</label>
+                            <input
+                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                type='email'
+                                name='email'
+                            />
+                            </div>
+                            <div className='flex flex-col py-2'>
+                            <label className='uppercase text-sm py-2'>Subject</label>
+                            <input
+                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                type='text'
+                                name='subject'
+                            />
+                            </div>
+                            <div className='flex flex-col py-2'>
+                            <label className='uppercase text-sm py-2'>Message</label>
+                            <textarea
+                                className='border-2 rounded-lg p-3 border-gray-300'
+                                rows='10'
+                                name='message'
+                            ></textarea>
+                            </div>
+                        </form>
                     </div>
-                    
                 </div>
             </div>
         </div>
