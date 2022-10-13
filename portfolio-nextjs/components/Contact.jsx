31761e1aa96d033203d08f58a69fd0ea2ea25fcf
mcrooks65 <mfcrooks@gmail.com>
2022-10-13 15:34:03 -0700

    Add message send button to form

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -85,13 +85,14 @@ const Contact = () => {
                             />
                             </div>
                             <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2'>Message</label>
-                            <textarea
-                                className='border-2 rounded-lg p-3 border-gray-300'
-                                rows='10'
-                                name='message'
-                            ></textarea>
+                                <label className='uppercase text-sm py-2'>Message</label>
+                                <textarea
+                                    className='border-2 rounded-lg p-3 border-gray-300'
+                                    rows='10'
+                                    name='message'
+                                ></textarea>
                             </div>
+                            <button className='w-full p-4 text-gray 100 mt-4'>Send Message</button>
                         </form>
                     </div>
                 </div>
