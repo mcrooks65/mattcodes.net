4e22b36d1e313e0914e261f3ea4cc40d35b0a2d2
mcrooks65 <mfcrooks@gmail.com>
2022-10-20 13:42:47 -0700

    Add technologies to flagnote project page

--- portfolio-nextjs/pages/flagnote.jsx
+++ portfolio-nextjs/pages/flagnote.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import FlagNoteImage from '../public/assets/projects/FlagNote-React.jpg'
+import {RiRadioButtonFill} from 'react-icons/ri'
 
 const flagnote = () => {
   return (
@@ -17,6 +18,34 @@ const flagnote = () => {
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>FlagNote</h2>
           <h3>Ruby on Rails / React JS / Tailwind</h3>
+        </div>
+      </div>
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
+        <div className='col-span-4'>
+            <p>Project</p>
+            <h2>Overview</h2>
+            <p>A web-based note taking application specifically for pentesters and CTF events.</p>
+            {/* TO ADD - Story behind project!  Why did you code it and what did you get out of it! */}
+            <ul>
+                <li>● Designed an interactive, efficient UI with RESTful routing using React-Redux.</li>
+                <li>● Developed Ruby on Rails API back-end to handle data persistence and respond to fetch requests.</li>
+                <li>● Applied custom Tailwind styling resulting in improved readability and use.</li>
+            </ul>
+            <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
+            <button className='px-8 py-2 mt-4'>Code</button>
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='p-2'>
+                <p>Technologies</p>
+            </div>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Ruby on Rails</p>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />HTML</p>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />JavaScript</p>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />React</p>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Redux</p>
+                <p className='text-gray-200 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Tailwind</p>
+            </div>
         </div>
       </div>
     </div>
