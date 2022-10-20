9d3d75a67ab1a0fbe15ec7d211fee1d22ee0280a
mcrooks65 <mfcrooks@gmail.com>
2022-10-20 12:29:22 -0700

    Create special navbar background for FlagNote landing page

--- portfolio-nextjs/pages/flagnote.jsx
+++ portfolio-nextjs/pages/flagnote.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import FlagNoteImage from '../public/assets/projects/FlagNote-React.jpg'
 
 const flagnote = () => {
   return (
-    <div>flagnote</div>
+    <div className='w-full'>
+        <div className='w-screen h-[30vh] lg:w-[40vh] relative'>
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/50 z-10' />
+            <Image className='absolute z-1' layout='fill' objectFit='cover' src={FlagNoteImage} alt='/' />
+        </div>
+
+    </div>
   )
 }
 
