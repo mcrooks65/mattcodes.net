e3225f9cd30047e90268a2ebb377a1da3aa7e27c
mcrooks65 <mfcrooks@gmail.com>
2022-11-18 16:50:09 -0800

    Add form endpoint for Contact component with getform.io

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -46,12 +46,12 @@ const Contact = () => {
                 {/* Right */}
                 <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                     <div className='py-4'>
-                        <form>
+                        <form action='https://getform.io/f/b0504036-d855-4890-af3e-08cbe27f2ee0' method='POST'>
                             <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                             <div className='flex flex-col'>
                                 <label className='uppercase text-xl py-2'>Name</label>
                                 <input
-                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                className='border-2 rounded-lg p-3 flex border-gray-300  text-black'
                                 type='text'
                                 name='name'
                                 />
@@ -61,7 +61,7 @@ const Contact = () => {
                                 Phone Number
                                 </label>
                                 <input
-                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
                                 type='text'
                                 name='phone'
                                 />
@@ -70,7 +70,7 @@ const Contact = () => {
                             <div className='flex flex-col py-2'>
                             <label className='uppercase text-xl py-2'>Email</label>
                             <input
-                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
                                 type='email'
                                 name='email'
                             />
@@ -78,7 +78,7 @@ const Contact = () => {
                             <div className='flex flex-col py-2'>
                             <label className='uppercase text-xl py-2'>Subject</label>
                             <input
-                                className='border-2 rounded-lg p-3 flex border-gray-300'
+                                className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
                                 type='text'
                                 name='subject'
                             />
@@ -86,12 +86,12 @@ const Contact = () => {
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-xl py-2'>Message</label>
                                 <textarea
-                                    className='border-2 rounded-lg p-3 border-gray-300'
+                                    className='border-2 rounded-lg p-3 border-gray-300 text-black'
                                     rows='10'
                                     name='message'
                                 ></textarea>
                             </div>
-                            <button className='w-full p-4 text-gray 100 mt-4 text-xl'>Send Message</button>
+                            <button type='submit' className='w-full p-4 text-gray 100 mt-4 text-xl'>Send Message</button>
                         </form>
                     </div>
                 </div>
